aac07f48f3cbbd8e58ac846b293b438b57c589fa
demosjarco <demosjarco@users.noreply.github.com>
2020-11-04 16:05:09 -0700

    Use shell for gen too

--- index.js
+++ index.js
@@ -62,8 +62,15 @@ function checkKeyType() {
 const spawn = require('child_process').spawn;
 
 function createEcdsaPrivKey() {
-	let openssl = spawn("openssl", ['ecparam', '-name', 'secp384r1', '-genkey', '-noout', '-out', fileName + '.key'], {
+	let openssl = spawn("openssl", [
+		'ecparam',
+		'-name secp384r1',
+		'-genkey',
+		'-noout',
+		'-out ' + fileName + '.key'
+	], {
 		cwd: '/tmp/',
+		shell: true,
 		windowsHide: true
 	});
 	openssl.stdout.setEncoding('utf8');
