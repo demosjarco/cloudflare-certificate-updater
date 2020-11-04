bc5911e4842f41a72f2b1952c174bf8fb1adcd0c
demosjarco <demosjarco@users.noreply.github.com>
2020-11-04 13:06:58 -0700

    Added function to make sure supported type of key

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 
 require('dotenv').config();
 const axios = require('axios').default;
+
+function checkKeyType() {
+	//const valid = ["origin-rsa", "origin-ecc"];
+	const valid = ["origin-ecc"];
+	const input = process.env.CLOUDFLARE_CERT_TYPE;
+	if (valid.includes(input)) {
+		return input;
+	} else {
+		throw new Error("Invalid key type. Valid choices: origin-rsa, origin-ecc");
+	}
+}
