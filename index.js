f1c61d4403c2735e2619e4f7e3113086ae1a2840
demosjarco <demosjarco@users.noreply.github.com>
2020-11-04 13:07:47 -0700

    Make sure key length is valid

--- index.js
+++ index.js
@@ -3,6 +3,16 @@
 require('dotenv').config();
 const axios = require('axios').default;
 
+function checkValidityLength() {
+	const valid = [7, 30, 90, 365, 730, 1095, 5475];
+	const input = parseInt(process.env.CLOUDFLARE_VALIDITY);
+	if (valid.includes(input)) {
+		return input;
+	} else {
+		throw new Error("Invalid validity length. Valid choices: 7, 30, 90, 365, 730, 1095, 5475")
+	}
+}
+
 function checkKeyType() {
 	//const valid = ["origin-rsa", "origin-ecc"];
 	const valid = ["origin-ecc"];
